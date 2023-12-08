3358b7f00f49d25adbcc8a2540cc432bddb7ca70
banchet18 <banjarac6@gmail.com>
2023-12-08 17:36:21 +0530

    changes

--- harkirat/javascript/variables.js
+++ harkirat/javascript/variables.js
@@ -1,7 +1,11 @@
 var name1 = "chetan"; // name is variable
 var age = 20;
 var users = ["harkirat", "raman", "samar"]; // array of name
-
+var users1 = {
+  name: "harkirat",
+  age: 20,
+};
 console.log(name1);
 console.log(age);
 console.log(users);
+console.log(users1);
